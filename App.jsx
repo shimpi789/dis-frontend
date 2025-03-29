b79164db5de806ab7ec86ad1208f47512af2fdbd
shimpi789 <shimpirajawat360@gmail.com>
2025-03-29 11:52:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login'; // Import the Login component
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} /> {/* Add route for Login */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
