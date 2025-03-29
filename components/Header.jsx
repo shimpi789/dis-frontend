19e3aa03b09e5c0664bac399f04482e12f048d52
shimpi789 <shimpirajawat360@gmail.com>
2025-03-29 11:41:55 +0530

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Disaster Info Aggregation Platform</h1>
+    </header>
+  );
+};
+
+export default Header;
